8f24efd885c877a7cb8312bb97d0025eef055f37
patilharshal2020 <patilharshal675@gmail.com>
2022-04-15 09:09:05 +0530

    electrical page carousel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ElectricalComponent } from '../app/electrical/electrical.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    { path: 'electrical', component: ElectricalComponent},
+    // { path: 'home', component: HomeComponent},
+    // { path: 'services', component: ServicesComponent },
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ElectricalComponent } from './electrical/electrical.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    ElectricalComponent
   ],
   imports: [
     BrowserModule,
